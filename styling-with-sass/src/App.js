a10b1069960311c1c51c99524ee9e73bbffa05a6
권은서 <kwonenseo@gmail.com>
2022-05-05 15:01:34 +0900

    feat: darkmode with scss

--- styling-with-sass/src/App.js
+++ styling-with-sass/src/App.js
@@ -1,40 +1,49 @@
 import "./App.scss";
+import "./styles/_theme.scss";
 import Button from "./components/Button";
+import Mode from "./components/Mode/Mode";
 
 function App() {
   return (
-    <div className="App">
-      <div className="buttons">
-        <Button size="large" onClick={() => console.log("clicked")}>
-          BUTTON
-        </Button>
-        <Button>BUTTON</Button>
-        <Button size="small" color="gray">
-          BUTTON
-        </Button>
-      </div>
-      <div className="buttons">
-        <Button size="large" color="pink">
-          BUTTON
-        </Button>
-        <Button>BUTTON</Button>
-        <Button size="small" color="gray" outline>
-          BUTTON
-        </Button>
-      </div>
-      <div className="buttons">
-        <Button color="pink" fullWidth>
-          BUTTON
-        </Button>
-        <Button outline fullWidth>
-          BUTTON
-        </Button>
-        <Button color="gray" fullWidth>
-          BUTTON
-        </Button>
-      </div>
+    <div className="app" data-theme="dark">
+      <Mode />
     </div>
   );
 }
+// function App() {
+//   return (
+//     <div className="App">
+//       <div className="buttons">
+//         <Button size="large" onClick={() => console.log("clicked")}>
+//           BUTTON
+//         </Button>
+//         <Button>BUTTON</Button>
+//         <Button size="small" color="gray">
+//           BUTTON
+//         </Button>
+//       </div>
+//       <div className="buttons">
+//         <Button size="large" color="pink">
+//           BUTTON
+//         </Button>
+//         <Button>BUTTON</Button>
+//         <Button size="small" color="gray" outline>
+//           BUTTON
+//         </Button>
+//       </div>
+//       <div className="buttons">
+//         <Button color="pink" fullWidth>
+//           BUTTON
+//         </Button>
+//         <Button outline fullWidth>
+//           BUTTON
+//         </Button>
+//         <Button color="gray" fullWidth>
+//           BUTTON
+//         </Button>
+//       </div>
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ styling-with-sass/src/components/Mode/Mode.js
@@ -0,0 +1,11 @@
+import "./Mode.scss";
+
+function Mode() {
+  return (
+    <div className="mode">
+      <h1 className="title">Light / Dark Mode</h1>
+      <span className="subtitle">Switch Colors</span>
+    </div>
+  );
+}
+export default Mode;
